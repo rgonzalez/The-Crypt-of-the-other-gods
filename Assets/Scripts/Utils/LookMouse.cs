3c9597cedf3bd1f063ba709f9ab12eabacacb4d1
Roberto <roberto.gonzalez@edosoft.es>
2020-10-15 23:30:07 +0100

    clean some assets, and add weapon

---
+++ Assets/Scripts/Utils/LookMouse.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookMouse : MonoBehaviour
+{
+
+    public Vector3 worldPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Plane plane = new Plane(Vector3.up, 0);
+
+        float distance;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (plane.Raycast(ray, out distance))
+        {
+            worldPosition = ray.GetPoint(distance);
+        }
+        // in the same Y axis
+        Vector3 pos = new Vector3(worldPosition.x, transform.position.y, worldPosition.z);
+        transform.LookAt(pos);
+    }
+}
