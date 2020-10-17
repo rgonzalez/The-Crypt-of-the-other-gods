0c692d49a6213c058f574232182faea993ff0ab0
Roberto <roberto.gonzalez@edosoft.es>
2020-10-17 14:54:35 +0100

    gun working

--- Assets/Scripts/Player/guns/LineWeapon.cs
+++ Assets/Scripts/Player/guns/LineWeapon.cs
@@ -9,6 +9,7 @@ public class LineWeapon : MonoBehaviour
     public GameObject muzzleFlashPrefab;
     public LineRenderer lineRenderer;
     private GameObject muzzleFlash;
+    private AudioSource audioSource;
 
     public float distance = 10f;
 
@@ -17,7 +18,21 @@ public class LineWeapon : MonoBehaviour
 
 
     private float nextFire = 0f;
-    private bool firing;
+    private bool firing = false;
+    private bool reload = false;
+    private bool reloading = false;
+
+
+    // weapon info
+    public int ammo = 30; // actual ammo in the clip
+    public int maxClip = 30; //max capacity of the clip
+    public int bulletsPerShoot = 1; // how much ammo per shoot
+    public float reloadSconds = 2;
+    public int damage = 2;
+
+    // Audios
+    public AudioClip shootAudio;
+    public AudioClip reloadAudio;
 
 
     // Start is called before the first frame update
@@ -29,12 +44,13 @@ public class LineWeapon : MonoBehaviour
             muzzleFlash = Instantiate(muzzleFlashPrefab, transform);
             muzzleFlash.SetActive(false);
         }
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Fire1"))
+        if (Input.GetButton("Fire1") && !reloading)
         {
 
             firing = true;
@@ -47,68 +63,110 @@ public class LineWeapon : MonoBehaviour
             //    muzzleFlash.SetActive(false);
             }
         }
+        if (Input.GetButton("Reload"))
+        {
+            reload = true;
+        } else
+        {
+            reload = false;
+        }
     }
 
     private void FixedUpdate()
     {
         if (firing)
         {
-            if (Time.time > nextFire)
+            if (ammo > 0)
             {
-                if (muzzleFlash != null)
-                {
-                    Debug.Log("ACTIVE!");
-                    muzzleFlash.SetActive(true);
-                    StartCoroutine(DisableMuzzle());
-                }
-
-                lineRenderer.SetVertexCount(2);
-                lineRenderer.SetPosition(0, transform.position);
-                Plane plane = new Plane(Vector3.up, transform.position);
-                float distance;
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                Vector3 worldPosition ;
-                if (plane.Raycast(ray, out distance))
-                {
-                    worldPosition = ray.GetPoint(distance);
-                    worldPosition = new Vector3(worldPosition.x, transform.position.y, worldPosition.z);
-                  
-                    Debug.Log("transform" + transform.position  + " worldPosition " + worldPosition);
-                    //   transform.LookAt(worldPosition);
-                    //  transform.TransformDirection(Vector3.forward) <- 2º parameter raycast
-                    RaycastHit hit;
-                    // Does the ray intersect any objects excluding the player layer
-                    if (Physics.Raycast(transform.position, worldPosition - transform.position, out hit, distance))
-                    {
-                        Debug.DrawRay(transform.position, worldPosition - transform.position, Color.yellow, 10f);
-                        lineRenderer.SetPosition(1, hit.point);
-                        Debug.Log("Did Hit");
-                    }
-                    else
-                    {
-                        Debug.DrawRay(transform.position, worldPosition - transform.position, Color.white, 10f);
-                        Debug.Log("Did not Hit " + hit.point);
-                        Vector3 pos = (worldPosition - transform.position) * distance;
-                        lineRenderer.SetPosition(1, pos);
-                    }
-                }
-                nextFire = Time.time + fireCD;
-            } else
-            {
-                lineRenderer.SetVertexCount(0);
-                if (muzzleFlash)
-                {
-                   // muzzleFlash.SetActive(false);
-                }
+                Shoot();
             }
-        } else
+        } 
+        if (reload)
         {
-            //lineRenderer.SetVertexCount(0);
+            if (!reloading)
+            {
+                Reload();
+            }
         }
     }
     IEnumerator DisableMuzzle()
     {
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(0.1f);
         muzzleFlash.SetActive(false);
+
+        lineRenderer.positionCount = 0;
+    }
+
+
+    void Shoot()
+    {
+        if (Time.time > nextFire)
+        {
+            if (muzzleFlash != null)
+            {
+                Debug.Log("ACTIVE!");
+                muzzleFlash.SetActive(true);
+                StartCoroutine(DisableMuzzle());
+            }
+            if (shootAudio)
+            {
+                audioSource.PlayOneShot(shootAudio);
+            }
+            ammo -= bulletsPerShoot;
+            lineRenderer.positionCount = 2;
+            lineRenderer.SetPosition(0, transform.position);
+            Plane plane = new Plane(Vector3.up, transform.position);
+            float distance;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldPosition;
+            if (plane.Raycast(ray, out distance))
+            {
+                worldPosition = ray.GetPoint(distance);
+                worldPosition = new Vector3(worldPosition.x, transform.position.y, worldPosition.z);
+
+                Debug.Log("transform" + transform.position + " worldPosition " + worldPosition);
+                //   transform.LookAt(worldPosition);
+                //  transform.TransformDirection(Vector3.forward) <- 2º parameter raycast
+                RaycastHit hit;
+                // Does the ray intersect any objects excluding the player layer
+                if (Physics.Raycast(transform.position, worldPosition - transform.position, out hit, distance))
+                {
+                   // Debug.DrawRay(transform.position, worldPosition - transform.position, Color.yellow, 10f);
+                    lineRenderer.SetPosition(1, hit.point);
+                    Debug.Log("Did Hit");
+                    if (hit.collider.CompareTag(Constants.TAG_ENEMY))
+                    {
+                        // try to check if has health to apply damage
+                        HealthScript enemyHealth = hit.collider.GetComponent<HealthScript>();
+                        if (enemyHealth) enemyHealth.Damage(damage);
+                    }
+                }
+                else
+                {
+                  //  Debug.DrawRay(transform.position, worldPosition - transform.position, Color.white, 10f);
+                    Debug.Log("Did not Hit " + hit.point);
+                    Vector3 pos = (worldPosition - transform.position) * distance;
+                    lineRenderer.SetPosition(1, pos);
+                }
+            }
+            nextFire = Time.time + fireCD;
+        } 
+    }
+
+    void Reload()
+    {
+        reloading = true;
+        if (reloadAudio)
+        {
+            audioSource.PlayOneShot(reloadAudio);
+        }
+        StartCoroutine(CompleteReload());
+    }
+
+    IEnumerator CompleteReload()
+    {
+        yield return new WaitForSecondsRealtime(reloadSconds);
+        reloading = false;
+        ammo = maxClip; //TODO: reload with real ammo from inventory
     }
 }
---
+++ Assets/Scripts/Utils/Constants.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants 
+{
+    // tags
+    public static string TAG_ENEMY = "Enemy";
+}
