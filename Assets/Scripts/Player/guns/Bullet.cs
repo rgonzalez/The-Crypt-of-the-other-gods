262949959e95f32706e92f8aa69e943251e8c5f7
Roberto <roberto.gonzalez@edosoft.es>
2020-10-18 14:35:23 +0100

    new bulletweapons

---
+++ Assets/Scripts/Player/guns/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public int damage = 1;
+    public float speed = 10;
+    public int bounces = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= transform.forward * Time.deltaTime * speed;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag(Constants.TAG_ENEMY))
+        {
+            // enemy damage
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Player/guns/BulletWeapon.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+///  Weapon that instantiate bullets, the bullet damage and specification can be defined in the bullet Prefab
+/// </summary>
+public class BulletWeapon : AbstractWeapon
+{
+
+    public GameObject bulletPrefab;
+    public int numberOfBullets = 1;
+
+
+    protected override void OnStarting()
+    {
+
+    }
+
+    protected override void OnUpdating()
+    {
+
+    }
+
+    protected override void Reload()
+    {
+        reloading = true;
+        if (reloadAudio)
+        {
+            audioSource.PlayOneShot(reloadAudio);
+        }
+        StartCoroutine(Completereload());
+    }
+
+    IEnumerator Completereload()
+    {
+        yield return new WaitForSecondsRealtime(reloadSconds);
+        reloading = false;
+        ammo = maxClip; //TODO: reload with real ammo from inventory
+    }
+
+    /// <summary>
+    /// Instantiate a new bullet to the mouse position
+    /// </summary>
+    protected override void Shoot()
+    {
+        if (Time.time > nextFire)
+        {
+            if (muzzleFlash != null)
+            {
+                muzzleFlash.SetActive(true);
+                StartCoroutine(DisableMuzzle());
+            }
+            if (shootAudio)
+            {
+                audioSource.PlayOneShot(shootAudio);
+            }
+            ammo -= bulletsPerShoot;
+            Plane plane = new Plane(Vector3.up, transform.position);
+            float distance;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldPosition;
+            if (plane.Raycast(ray, out distance))
+            {
+                worldPosition = ray.GetPoint(distance);
+                worldPosition = new Vector3(worldPosition.x, transform.position.y, worldPosition.z);
+
+                Debug.Log("transform" + transform.position + " worldPosition " + worldPosition);
+                //   transform.LookAt(worldPosition);
+                //  transform.TransformDirection(Vector3.forward) <- 2º parameter raycast
+                RaycastHit hit;
+
+
+                // now we have to cast a raycast per bullet (lineRenderer) with recoil
+                Vector3 target = worldPosition - transform.position; //original Target position
+                for (int i = 0; i < numberOfBullets; i++)
+                {
+                    // instantiate a new bullet with the recoil
+                    Vector3 newTarget = new Vector3(target.x + Random.Range(-recoilX, +recoilX), target.y + Random.Range(-recoilY, +recoilY), target.z + Random.Range(-recoilX, +recoilX));
+                    Quaternion relativeRotation = Quaternion.LookRotation(newTarget, Vector3.up); // get the rotation to the new target
+                    GameObject newBullet = Instantiate(bulletPrefab, transform.position, relativeRotation);
+                    
+                }
+
+            }
+        }
+    }
+
+    IEnumerator DisableMuzzle()
+    {
+        yield return new WaitForSeconds(0.1f);
+        muzzleFlash.SetActive(false);
+    }
+}
