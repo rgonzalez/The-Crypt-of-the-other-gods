3f4f4e62e8d6db248211cdff0eee2c600a3b287f
Roberto <roberto.gonzalez@edosoft.es>
2020-10-19 00:26:16 +0100

    new weapons

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Explosion that check if there is colliders in range 
+/// </summary>
+public class Explosion : MonoBehaviour
+{
+    // the force to aplly to push the objects, if this var has a value !=0, a force will push objects
+    public float powerForce = 0f;
+    public float radius = 1f;
+    public int damage = 0; // the damage can be transfered by the bullet in case of explosion by bullet/rocket
+
+    /// <summary>
+    /// Some explosions can be in time DPS (tics per seconds) or just in 1 explosion
+    /// so if we specify time explosion, will do Damage (by bullet) in every Tic
+    /// </summary>
+    /// 
+    public bool dps = false;
+    public float damageSeconds = 0f; //every X apply a damage
+    public float maxTime = 0f; // how much time is this explosion active
+
+
+    private float nextDamage = 0f;
+    public GameObject bullet; //sometimes the bullet pass itself to the explosion to be destroyed
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (damageSeconds == 0)
+        {
+            //only at the start if is a normal explosion
+            ApplyExplosion();
+        }
+        if (maxTime > 0)
+        {
+            StartCoroutine(DestroyAll());
+        }
+    }
+
+    private void Update()
+    {
+        if (damageSeconds > 0)
+        { 
+            if (nextDamage < Time.time)
+            {
+                ApplyExplosion();
+            }
+
+        }
+    }
+
+
+    // explosion logic
+    private void ApplyExplosion()
+    {
+        Debug.Log("BANG!");
+
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius);
+        foreach (Collider hitCollider in hitColliders)
+        {
+            hitCollider.SendMessage("Damage", damage); //send the message to addDamage to all items/enemys in the range
+            //also add Force of explosion
+            if (powerForce != 0)
+            {
+                Rigidbody rb = hitCollider.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    rb.AddExplosionForce(powerForce, transform.position, radius, 13f);
+                }
+            }
+        }
+        if (damageSeconds > 0)
+        {
+            nextDamage = Time.time + damageSeconds;
+        }
+
+    }
+
+
+    IEnumerator DestroyAll()
+    {
+        yield return new WaitForSecondsRealtime(maxTime);
+        if (bullet != null)
+        {
+            Destroy(bullet);
+        }
+        Destroy(gameObject);
+    }
+
+    /// <summary>
+    /// Draw the Gizmo of the explosion
+    /// </summary>
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, radius);
+    }
+}
--- Assets/Scripts/Player/guns/Bullet.cs
+++ Assets/Scripts/Player/guns/Bullet.cs
@@ -8,11 +8,28 @@ public class Bullet : MonoBehaviour
     public int damage = 1;
     public float speed = 10;
     public int bounces = 0;
+    private int actualBounces = 0;
+    public GameObject explosionPrefab; // if the bullet can explode, set here the prefab of the explosion, if not
+                                       // the bullet will hit 
+
+    public bool keepAfterExplosion = false; //sometimes we need the bullet keeps after explosion (like a mine)
+                                            // turn true to keep the bullet gameObject, but remember deactivate in the explotion
+
+    private RFX4_PhysicsMotion physicsMotion; // maybe the bullet is created by the prefab FX
+    // this package use custom collision system, we must add it to our script
+
+    private bool hasExploded = false; // with explosions in long time that keeps the grenade active 
+                                        // this script disables itself, but sometimes multiple updates are fired before
+                                        //so this bullet keeps instantiating explosions, this var controls this situation
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        physicsMotion = GetComponentInChildren<RFX4_PhysicsMotion>(true);
+        if (physicsMotion != null) physicsMotion.CollisionEnter += CollisionEnter;
+
+        var raycastCollision = GetComponentInChildren<RFX4_RaycastCollision>(true);
+        if (raycastCollision != null) raycastCollision.CollisionEnter += CollisionEnter;
     }
 
     // Update is called once per frame
@@ -21,12 +38,87 @@ public class Bullet : MonoBehaviour
         transform.position -= transform.forward * Time.deltaTime * speed;
     }
 
+    //normal collision
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag(Constants.TAG_ENEMY))
+        Collide(collision.gameObject, collision);
+    }
+
+
+    //collision by RFX4 package
+    private void CollisionEnter(object sender, RFX4_PhysicsMotion.RFX4_CollisionInfo e)
+    {
+
+        if (bounces > 0)
         {
-            // enemy damage
+            if (actualBounces < bounces)
+            {
+                // is an bouncing bullet, check if is the last bouncing or must keep bouncing
+                ((RFX4_PhysicsMotion)sender).canExplode = false;
+             
+            } else
+            {
+                // the RFX4 can explode the bullet, is not bouncing anymore
+                ((RFX4_PhysicsMotion)sender).canExplode = true;
+            }
+        } else
+        {
+            ((RFX4_PhysicsMotion)sender).canExplode = true;
         }
-        Destroy(gameObject);
+        Collide(e.HitGameObject, e.Collision);
+    }
+
+    private void Collide(GameObject other, Collision collision )
+    {
+        if (actualBounces < bounces && bounces > 0)
+        {
+            //is bouncing, change the normal direction
+            Vector3 newDirection = Vector3.Reflect(transform.forward, collision.contacts[0].normal);
+            newDirection.y = 0;
+            // we keep the same vertical value, with some recoilY, the bounces can be in discontrol bouncing to the ceiling
+            transform.rotation = Quaternion.LookRotation(newDirection);
+            actualBounces++;
+        } else 
+        {
+            // the actual bullet can do Damage, or can have a explosion Prefab to instantiate (in case of being a explosive bullet)
+            if (explosionPrefab)
+            {
+                if (hasExploded) // this bullet has exploded? then do  nothing until destroy
+                {
+                    return;
+                }
+                // instantiate the explosion, the explosion has the logic to damage
+                GameObject explode = Instantiate(explosionPrefab, transform.position,new Quaternion(0,0,0,0));
+                // if the explosion has a Explosion Script we will send the Damage to the explotion
+                Explosion explosionScript = explode.GetComponent<Explosion>();
+                if (explosionScript)
+                {
+                    explosionScript.damage = this.damage;
+                    if (keepAfterExplosion)
+                    {
+
+                        //pass the gameObject to the explosion, to destroy it in the explosionScript
+                        explosionScript.bullet = this.gameObject;
+                        //but we have to deactivate the grenadeScript, or it will keep spamming explosions
+                        this.enabled = false;
+                    }
+                }
+                hasExploded = true;
+            }
+            else
+            {
+                //the bullet dont explode, just hit enemy or walls
+                if (other.CompareTag(Constants.TAG_ENEMY))
+                {
+                    // enemy damage
+
+                }
+            }
+            if (!keepAfterExplosion)
+            {
+                Destroy(gameObject);
+            } 
+        }
+
     }
 }
